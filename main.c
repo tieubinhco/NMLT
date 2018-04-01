8ac60e8bd00e20fcbee18390e54c0af2600e8b5f
A2 <tieubinhco@gmail.com>
2018-04-02 00:02:22 +0700

    LAB 1.1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+
+
+
+void main()
+{
+    int so;
+    printf("Hay nhap mot so: ");
+    scanf("%d", &so);
+
+    printf("\nHexadecimal number   %X\n", so);
+    printf("Octal number         %o\n", so);
+    printf("Decimal number       %d\n", so);
+
+}
+
